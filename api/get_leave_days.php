7a2cb27e00ea919d073e39d8ccfe3ce468bd41f0
aunefyren <oystein.sverre@gmail.com>
2022-02-16 20:07:36 +0100

    Leave days

--- api/get_leave_days.php
+++ api/get_leave_days.php
@@ -18,6 +18,7 @@ $db = $database->getConnection();
 // instantiate product object
 $user = new User($db);
 $goal = new Goal($db);
+$exercise = new Exercise($db);
 $data = json_decode(file_get_contents("php://input"));
 
 // If POST data is empty or wrong
@@ -41,28 +42,24 @@ if(!$cookie_object) {
 
 }
 
-$cookie_decoded = json_decode($cookie_object, true);
-$goal->user_id = $cookie_decoded['data']['user_id'];
+$all_goals = $goal->get_goals_all();
 
-$now = new DateTime('NOW');
+if(!$all_goals) {
 
-if($now->format('m') == 1 || $now->format('m') == 8) {
-    $can_compete = true;
-} else {
-    $can_compete = false;
+    echo json_encode(array("error" => true, "message" => "Ingen mål funnet."));
+    exit(0);
+
 }
+
+$all_goals = json_decode($all_goals, true);
 
 $goal_dates = $goal->get_current_season_dates();
 
 if(!$goal_dates) {
 
-    echo json_encode(array( "error" => false, 
-                            "message" => "Ingen mål funnet. Ingen sesong funnet.",
-                            "can_compete" => $can_compete, 
-                            "season_start" => false,
-                            "season_end" => false,
-                            "season_name" => false,
-                            "goal" => false));
+    echo json_encode(array( "error" => true, 
+                            "message" => "Fant ikke nåværende sesong."
+                        ));
     exit(0);
 
 }
@@ -70,43 +67,123 @@ if(!$goal_dates) {
 $goal_dates = json_decode($goal_dates, true);
 $season_start = new DateTime($goal_dates['season_start']);
 $season_end = new DateTime($goal_dates['season_end']);
-$season_name = $goal_dates['season_name'];
 
-$goals = $goal->get_goal($season_start, $season_end);
+$goal_id_list = array();
 
-if($goals === false) {
+$now = new DateTime('NOW');
 
-    echo json_encode(array( "error" => false, 
-                            "message" => "Ingen mål funnet.",
-                            "can_compete" => $can_compete, 
-                            "season_start" => $season_start->format('d.m.Y'),
-                            "season_end" => $season_end->format('d.m.Y'),
-                            "season_name" => $season_name,
-                            "goal" => false));
-    exit(0);
-
-} else {
-
-    $goals = json_decode($goals, true);
-
-    echo json_encode(array( "error" => false, 
-                            "message" => "Fant et mål.", 
-                            "can_compete" => $can_compete, 
-                            "season_start" => $season_start->format('d.m.Y'),
-                            "season_end" => $season_end->format('d.m.Y'),
-                            "season_name" => $season_name,
-                            "goal" => 
-                                array(
-                                    "goal_id" => $goals['goal']['goal_id'],
-                                    "goal_exer_week" => $goals['goal']['goal_exer_week'],
-                                    "goal_start" => $goals['goal']['goal_start'],
-                                    "goal_end" => $goals['goal']['goal_end'],
-                                    "goal_compete" => $goals['goal']['goal_compete'],
-                                    "goal_started" => $goals['goal']['goal_started']
-                                )
-                            ));
-    exit(0);
-
+for($i = 0; $i < count($all_goals); $i++) {
+    $goal_start = date_create_from_format('Y-m-d H:i:s', $all_goals[$i]['goal_start']);
+    $goal_end = date_create_from_format('Y-m-d H:i:s', $all_goals[$i]['goal_end']);
+    $user_id = $all_goals[$i]['user_id'];
+    if($goal_start >= $season_start && $goal_end <= $season_end && ($goal_start->format('Y-m-d') == $now->format('Y-m-d') || $goal_start < $now) && $user_id == $cookie_object->user_id) {
+        array_push($goal_id_list, array('goal_id' => $all_goals[$i]['goal_id'], 'user_id' => $all_goals[$i]['user_id'], 'exercise_found' => false));
+    }
 }
 
+for($i = 0; $i < count($goal_id_list); $i++) {
+    $exercise->goal_id = $goal_id_list[$i]['goal_id'];
+    $data = $exercise->get_exercises_stats();
+
+    if(!$data) {
+
+        $goal_id_list[$i]['exercise_found'] = false;
+        $goal_id_list[$i]['user_firstname'] = false;
+        $goal_id_list[$i]['user_lastname'] = false;
+        $goal_id_list[$i]['goal_start'] = false;
+        $goal_id_list[$i]['goal_end'] = false;
+        $goal_id_list[$i]['goal_exer_week'] = false;
+        $goal_id_list[$i]['week_complete'] = false;
+        $goal_id_list[$i]['week_percent'] = 0;
+        $goal_id_list[$i]['goal_compete'] = false;
+        $goal_id_list[$i]['streak'] = 0;
+        $goal_id_list[$i]['week_leave'] = false;
+
+        $goal->goal_id = $goal_id_list[$i]['goal_id'];
+        $goal_data = $goal->get_goal_user();
+
+        if(!$goal_data) {
+
+            echo json_encode(array("error" => true, "message" => "Feilet i å finne brukerdata for mål."));
+            exit(0);
+
+        }
+
+        $goal_data = json_decode($goal_data, true);
+
+        $goal_id_list[$i]['user_firstname'] = $goal_data[0]['user_firstname'];
+        $goal_id_list[$i]['user_lastname'] = $goal_data[0]['user_lastname'];
+        $goal_id_list[$i]['goal_start'] = $goal_data[0]['goal_start'];
+        $goal_id_list[$i]['goal_end'] = $goal_data[0]['goal_end'];
+        $goal_id_list[$i]['goal_exer_week'] = $goal_data[0]['goal_exer_week'];
+        $goal_id_list[$i]['goal_compete'] = $goal_data[0]['goal_compete'];
+
+    } else {
+        $data = json_decode($data, true);
+
+        //print_r($data);
+
+        $goal_id_list[$i]['exercise_found'] = true;
+        $goal_id_list[$i]['user_firstname'] = $data[0]['user_firstname'];
+        $goal_id_list[$i]['user_lastname'] = $data[0]['user_lastname'];
+        $goal_id_list[$i]['goal_start'] = $data[0]['goal_start'];
+        $goal_id_list[$i]['goal_end'] = $data[0]['goal_end'];
+        $goal_id_list[$i]['goal_exer_week'] = $data[0]['goal_exer_week'];
+        $goal_id_list[$i]['goal_compete'] = $data[0]['goal_compete'];
+        $goal_id_list[$i]['week_complete'] = false;
+        $goal_id_list[$i]['week_percent'] = 0;
+        $goal_id_list[$i]['streak'] = 0;
+        $goal_id_list[$i]['week_leave'] = false;
+
+        $weeks = array_fill(0, 52, 0);
+
+        for($j = 0; $j < count($data); $j++) {
+            $exer_date = new DateTime($data[$j]['exer_date']);
+            $week = intval($exer_date->format('W'));
+
+            if($data[$j]['exer_leave']) {
+                $weeks[$week] = NULL;
+            } else if($weeks[$week] !== NULL) {
+                $weeks[$week] = intval($weeks[$week]) + 1;
+            }
+        }
+
+        $streak = 0;
+        
+        $goal_start = DateTime::createFromFormat('Y-m-d H:i:s', $goal_id_list[$i]['goal_start']);
+        $start_week = intval($goal_start->format('W'));
+
+        $goal_end = DateTime::createFromFormat('Y-m-d H:i:s', $goal_id_list[$i]['goal_end']);
+        $end_week = intval($goal_end->format('W'));
+
+        $now = new DateTime('NOW');
+        $current_week = intval($now->format('W'));
+
+        for($j = $start_week; $j <= $current_week-1; $j++) {
+            if($weeks[$j] !== NULL && $weeks[$j] >= $goal_id_list[$i]['goal_exer_week']) {
+                $streak++;
+            } else if($weeks[$j] !== NULL){
+                $streak = 0;
+            }
+
+        }
+
+        if($weeks[$current_week] === NULL) {
+            $goal_id_list[$i]['week_leave'] = true;
+        }
+
+        $goal_id_list[$i]['streak'] = $streak;
+
+        if($weeks[$current_week] >= $goal_id_list[$i]['goal_exer_week'] || $weeks[$current_week] === NULL) {
+            $goal_id_list[$i]['week_complete'] = true;
+        } else {
+            $goal_id_list[$i]['week_complete'] = false;
+        }
+
+        $goal_id_list[$i]['week_percent'] = $weeks[$current_week] / $goal_id_list[$i]['goal_exer_week'] * 100;
+    }
+}
+
+echo json_encode(array("error" => false, "message" => "Fant trening for uken.", "users" => $goal_id_list));
+exit(0);
 ?>
