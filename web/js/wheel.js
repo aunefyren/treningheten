5eee467f35906c59fa1b72fd040120f7a3e4f8be
aunefyren <oystein.sverre@gmail.com>
2023-03-07 17:11:28 +0100

    Removed color

--- web/js/wheel.js
+++ web/js/wheel.js
@@ -161,7 +161,7 @@ function get_debt(debt_id) {
 function placeWheel(canidateArray) {
 
     var colors = [
-        "#800000", "#9A6324", "#808000", "#469990", "#000075", "#e6194B", "#f58231", "#ffe119", "#bfef45", "#3cb44b", "#42d4f4", "#4363d8", "#911eb4", "#f032e6", "#a9a9a9", "#fabed4", "#ffd8b1", "#fffac8", "#aaffc3", "#dcbeff", "#ffffff"
+        "#800000", "#9A6324", "#808000", "#469990", "#e6194B", "#f58231", "#ffe119", "#bfef45", "#3cb44b", "#42d4f4", "#4363d8", "#911eb4", "#f032e6", "#a9a9a9", "#fabed4", "#ffd8b1", "#fffac8", "#aaffc3", "#dcbeff", "#ffffff"
     ]
 
     document.getElementById('wheel-module').style.display = "flex";
