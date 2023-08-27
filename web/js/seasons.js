1dab446943c141469fbaa875bb967155f7558233
aunefyren <oystein.sverre@gmail.com>
2023-08-27 20:44:51 +0200

    CSS changes
    moved stats

--- web/js/seasons.js
+++ web/js/seasons.js
@@ -41,6 +41,43 @@ function load_page(result) {
                     </div>
 
                 </div>
+
+                <div class="module color-invert">
+                    <hr>
+                </div>
+
+                <div class="module" id="stats_module">
+
+                    <div class="title">
+                        Season statistics
+                    </div>
+
+                    <div class="form-group">
+                        <select id='select_season' class='form-control' onchange="choose_season()">
+                            <option value="null">Choose season</option>
+                        </select>
+                    </div>
+
+                    <div>
+
+                        <div class="module" id="loading-dumbell" style="display: none;">
+                            <img src="./assets/images/barbell.gif">
+                        </div>
+
+                        <div id="season-statistics-element-wrapper-div" class="season-statistics-element-wrapper-div">
+                        </div>
+
+                        <div id="chart-canvas-div" style="max-width: 40em; margin: 1em auto; padding: 0 0.5em;">
+                            <canvas id="myChart" style="max-width: 100%; width: 1000px; display:none;"></canvas>
+                        </div>
+
+                        <div id="chart-canvas-div-two" style="max-width: 40em; margin: 1em auto; padding: 0 0.5em;">
+                            <canvas id="myChartTwo" style="max-width: 100%; width: 1000px; display:none;"></canvas>
+                        </div>
+
+                    </div>
+
+                </div>
     `;
 
     document.getElementById('content').innerHTML = html;
@@ -84,6 +121,7 @@ function get_seasons(){
 
                 console.log("Placing intial seasons: ")
                 place_seasons(seasons);
+                place_seasons_input(seasons);
 
             }
 
@@ -367,5 +405,349 @@ function GetProfileImageForUserOnLeaderboard(userID, seasonID) {
 function PlaceProfileImageForUserOnLeaderboard(imageBase64, userID, seasonID) {
 
     document.getElementById("member-img-" + seasonID + "-" + userID).src = imageBase64
+
+}
+
+function place_seasons_input(seasons_array) {
+
+    var select_season = document.getElementById("select_season");
+    var seasons = []
+    var now = new Date();
+
+    for(var i = 0; i < seasons_array.length; i++) {
+        var user_found = false;
+        for(var j = 0; j < seasons_array[i].goals.length; j++) {
+            if(seasons_array[i].goals[j].user.ID == user_id) {
+                user_found = true;
+                break
+            }
+        }
+
+        var futureSeason = true;
+        try {
+            var season_start_object = new Date(seasons_array[i].start);
+            if(now > season_start_object) {
+                futureSeason = false;
+            }
+        } catch(e) {
+            console.log("Failed to parse season start. Error: " + e)
+        }
+
+        if(user_found && !futureSeason) {
+            seasons.push(seasons_array[i])
+        }
+    }
+
+    for(var i = 0; i < seasons.length; i++) {
+        
+        var option = document.createElement("option");
+        option.text = seasons[i].name
+        option.value = seasons[i].ID
+        select_season.add(option); 
+
+    }
+
+}
+
+function choose_season() {
+
+    var select_season = document.getElementById("select_season");
+
+    // Show loading gif
+    document.getElementById("loading-dumbell").style.display = "inline-block";
+
+    // Purge data
+    canvas_div = document.getElementById("chart-canvas-div");
+    canvas_div.innerHTML = "";
+    canvas_div.innerHTML = '<canvas id="myChart" style="max-width: 100%; width: 1000px; display:none;"></canvas>';
+
+    canvas_div_two = document.getElementById("chart-canvas-div-two");
+    canvas_div_two.innerHTML = "";
+    canvas_div_two.innerHTML = '<canvas id="myChartTwo" style="max-width: 100%; width: 1000px; display:none;"></canvas>';
+
+    document.getElementById("season-statistics-element-wrapper-div").innerHTML = "";
+
+    if(select_season.value == null || select_season.value == 0 || select_season.value == "null") {
+
+        // Show loading gif
+        document.getElementById("loading-dumbell").style.display = "none";
+
+        var myChartElement = document.getElementById("myChart");
+        myChartElement.style.display = "none"
+
+    } else {
+        
+        get_season_leaderboard(select_season.value)
+
+    }
+
+}
+
+function get_season_leaderboard(seasonID){
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            
+            try {
+                result = JSON.parse(this.responseText);
+            } catch(e) {
+                console.log(e +' - Response: ' + this.responseText);
+                error("Could not reach API.");
+                return;
+            }
+            
+            if(result.error) {
+
+                error(result.error);
+
+            } else {
+
+                place_statistics(result.leaderboard, result.weekdays, result.wheel_statistics);
+                
+            }
+
+        } else {
+            // info("Loading week...");
+        }
+    };
+    xhttp.withCredentials = true;
+    xhttp.open("post", api_url + "auth/season/" + seasonID + "/leaderboard-personal");
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.setRequestHeader("Authorization", jwt);
+    xhttp.send();
+    return false;
+
+}
+
+function place_statistics(leaderboard_array, weekday_array, wheel_statistics) {
+
+    var myChartElement = document.getElementById("myChart");
+    myChartElement.style.display = "inline-block"
+
+    var myChartElement = document.getElementById("myChartTwo");
+    myChartElement.style.display = "inline-block"
+
+    leaderboard_array = leaderboard_array.reverse();
+
+    var xValues = [];
+    var yValues = [];
+    var goals = [];
+    var pointBackgroundColorArray = [];
+    var borderColorArray = [];
+    var longest_streak = 0;
+    var highest_week = 0;
+    var exercise_amount = 0;
+    var sickleave_amount = 0;
+    var goal = 0;
+    var week_count = 0;
+    var complete_weeks = 0;
+    var incomplete_weeks = 0;
+    var wheels_won = wheel_statistics.wheels_won;
+    var wheels_lost = wheel_statistics.wheel_spins;
+
+    // Look through array of data
+    for (var i = 0; i < leaderboard_array.length; i++) {
+
+        xValues.push("" + leaderboard_array[i].week_number + " (" + leaderboard_array[i].week_year + ")");
+
+        var exercise = leaderboard_array[i].user.week_completion_interval
+        var sickleave = leaderboard_array[i].user.sickleave
+        goal = leaderboard_array[i].user.exercise_goal
+        var streak = leaderboard_array[i].user.current_streak
+        exercise_amount = exercise_amount + exercise
+
+        if(streak > longest_streak) {
+            longest_streak = streak;
+        }
+
+        if(exercise > highest_week) {
+            highest_week = exercise;
+        }
+        
+        if(sickleave) {
+            pointBackgroundColorArray.push("rgba(215, 20, 20, 1)")
+            borderColorArray.push("rgba(215, 20, 20, 1)")
+            sickleave_amount = sickleave_amount + 1
+        } else {
+            pointBackgroundColorArray.push("rgba(119,141,169,1)")
+            borderColorArray.push("rgba(119,141,169,1)")
+        }
+
+        yValues.push(eval(exercise));
+        goals.push(eval(goal));
+
+        if(exercise >= goal) {
+            complete_weeks = complete_weeks + 1
+        } else {
+            incomplete_weeks = incomplete_weeks + 1
+        }
+
+        week_count = week_count + 1
+           
+    }
+
+    console.log("goal: " + goal)
+    console.log("weeks: " + week_count)
+    console.log("complete weeks: " + complete_weeks)
+
+    week_completion_percentage = Math.floor((complete_weeks / week_count) * 100)
+
+    const lineChart = new Chart("myChart", {
+        type: "line",
+        data: {
+            labels: xValues,
+            datasets: [
+                {
+                    fill: true,
+                    borderColor: borderColorArray,
+                    pointBackgroundColor: pointBackgroundColorArray,
+                    backgroundColor: "rgba(119,141,169,0.5)",
+                    responsive: true,
+                    data: yValues,
+                    tension: 0,
+                    label: "Exercise count",
+                },
+                {
+                    fill: true,
+                    borderColor: "rgba(119,141,169,0.25)",
+                    responsive: false,
+                    data: goals,
+                    tension: 0,
+                    label: "Goal",
+                }
+            ]
+        },    
+        options: {
+            legend: {display: false},
+            title: {
+                display: true,
+                text: "Weekly exercise graph",
+                fontSize: 16
+            },
+            scales: {
+                yAxes: [
+                    {
+                        beginAtZero: true,
+                        min: 0,
+                        ticks: {
+                            beginAtZero: true,
+                            precision: 0
+                        }
+                    }
+                ]
+            }
+        }
+    });
+
+
+    var xValues2 = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    var yValues2 = [weekday_array.monday, weekday_array.tuesday, weekday_array.wednesday, weekday_array.thursday, weekday_array.friday, weekday_array.saturday, weekday_array.sunday]
+
+    const lineChartTwo = new Chart("myChartTwo", {
+        type: "line",
+        data: {
+            labels: xValues2,
+            datasets: [
+                {
+                    fill: true,
+                    borderColor: "rgba(119,141,169,1)",
+                    pointBackgroundColor: "rgba(119,141,169,1)",
+                    backgroundColor: "rgba(119,141,169,0.5)",
+                    responsive: true,
+                    data: yValues2,
+                    tension: 0,
+                    label: "Exercise count",
+                }
+            ]
+        },    
+        options: {
+            legend: {display: false},
+            title: {
+                display: true,
+                text: "Weekday exercise graph",
+                fontSize: 16
+            },
+            scales: {
+                yAxes: [
+                    {
+                        beginAtZero: true,
+                        min: 0,
+                        ticks: {
+                            beginAtZero: true,
+                            precision: 0
+                        }
+                    }
+                ]
+            }
+        }
+    });
+
+    if(goal > 0) {
+        document.getElementById("season-statistics-element-wrapper-div").innerHTML += `
+            <div class="season-statistics-element unselectable">
+                Weekly exercise goal: ${goal}🏆
+            </div>
+        `;
+    }
+
+    if(week_completion_percentage > 0) {
+        document.getElementById("season-statistics-element-wrapper-div").innerHTML += `
+            <div class="season-statistics-element unselectable">
+                Average weekly goal completion: ${week_completion_percentage}%📊
+            </div>
+        `;
+    }
+
+    if(longest_streak > 0) {
+        document.getElementById("season-statistics-element-wrapper-div").innerHTML += `
+            <div class="season-statistics-element unselectable">
+                Longest week streak: ${longest_streak}🔥
+            </div>
+        `;
+    }
+
+    if(highest_week > 0) {
+        document.getElementById("season-statistics-element-wrapper-div").innerHTML += `
+            <div class="season-statistics-element unselectable">
+                Most exercise in a week: ${highest_week}🏋️
+            </div>
+        `;
+    }
+
+    if(exercise_amount > 0) {
+        document.getElementById("season-statistics-element-wrapper-div").innerHTML += `
+            <div class="season-statistics-element unselectable">
+                All exercise combined: ${exercise_amount}💰
+            </div>
+        `;
+    }
+
+    if(sickleave_amount > 0) {
+        document.getElementById("season-statistics-element-wrapper-div").innerHTML += `
+            <div class="season-statistics-element unselectable">
+                Weeks of sick leave: ${sickleave_amount}🤢
+            </div>
+        `;
+    }
+
+    if(wheels_lost > 0) {
+        document.getElementById("season-statistics-element-wrapper-div").innerHTML += `
+            <div class="season-statistics-element unselectable">
+                Wheels spun: ${wheels_lost}🎡
+            </div>
+        `;
+    }
+
+    if(wheels_won > 0) {
+        document.getElementById("season-statistics-element-wrapper-div").innerHTML += `
+            <div class="season-statistics-element unselectable">
+                Wheels won: ${wheels_won}⭐
+            </div>
+        `;
+    }
+    
+    // Remove loading gif
+    document.getElementById("loading-dumbell").style.display = "none";
 
 }
